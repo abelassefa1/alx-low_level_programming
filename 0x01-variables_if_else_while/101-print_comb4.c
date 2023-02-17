7ec702b441d87454dd6fea9eb2293ad46a017353
abelassefa1 <abelassefa24@gmail.com>
2023-02-17 12:30:53 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -5,6 +5,7 @@
  * Description: Write a program that prints all possible
  * different combinations of two digits
  * Numbers must be separated by ,, followed by a space
+ * Return: 0
  */
 int main(void)
 {
