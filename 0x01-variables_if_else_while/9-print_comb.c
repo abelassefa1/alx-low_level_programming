b232f18c4ccf521a3715ba9e343287dd407181a8
abelassefa1 <abelassefa24@gmail.com>
2023-02-17 09:17:18 -0800

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,49 +1,21 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
+ * main - Entry point 
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int a;
 
-	while (p < 58)
+	for (a = 0; a < 10; a++)
 	{
-		while (q < 58)
+		putchar(a + '0');
+		if (a < 9)
 		{
-			while (y < 58)
-			{
-				while (x < 58)
-				{
-					putchar(p);
-					putchar(q);
-					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
-				}
-				y++;
-				x = 48;
-			}
-			q++;
-			q = p;
-			x = q + 1;
+			putchar(',');
+			putchar(',');
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
 	}
 	putchar('\n');
 	return (0);
