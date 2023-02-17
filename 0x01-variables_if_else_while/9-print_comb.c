e05a9cebe426b59255dc644ce32b9fcf28592019
abelassefa1 <abelassefa24@gmail.com>
2023-02-17 13:09:50 -0800

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/* more headers goes there *
- * main - Entry point
- *
- * Return: Always 0 (Sucess)
+
+/* betty style doc for function main goes there
+  * main - Entry point
+ * Return: Always 0 (Success)
  */
-int main()
+int main(void)
 {
 	int s = 0, e = 9;
 
