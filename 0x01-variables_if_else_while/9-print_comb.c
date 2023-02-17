5b204d42ddf9b613d3ac2f594698e7fda01c71b8
abelassefa1 <abelassefa24@gmail.com>
2023-02-17 13:04:43 -0800

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,15 +6,20 @@
  */
 int main()
 {
-	int i;
-	while (i <=5)
+	int s = 0, e = 9;
+
+	while (s <= e)
+
 	{
-		putchar(i+'0');
-
-			i++;
-
+		putchar(s +'0');
+		if (s!=e)
+		{
 		putchar(',');
 		putchar(' ');
 	}
+		s++;
+	}
+	putchar('\n');
+
 	return 0;
 }
